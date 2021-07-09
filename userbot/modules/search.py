55cdb33ff78a4955fac315b27bd81cdac6998cae
arshsisodiya <arshsisodiya@gmail.com>
2021-07-10 04:26:29 +0530

    fixup

--- userbot/modules/search.py
+++ userbot/modules/search.py
@@ -14,7 +14,6 @@ import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
-from main_startup.core.decorators import friday_on_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 
 
