c88c5a0e6b40c7042efbb50a46e909e0a22497a8
arshsisodiya <arshsisodiya@gmail.com>
2021-07-10 07:40:14 +0530

    fixup showing file size in gdlist

--- userbot/modules/gdrive.py
+++ userbot/modules/gdrive.py
@@ -778,11 +778,11 @@ async def lists(gdrive):
             if files.get("mimeType") == "application/vnd.google-apps.folder":
                 link = files.get("webViewLink")
                 message += f"📁️ • [{file_name}]({link})\n"
-                message += f"`Size  :` `{humanbytes(f_size)}`\n"
+                message += f"`Size   :` `{humanbytes(result[0])}`\n"
             else:
                 link = files.get("webContentLink")
                 message += f"📄️ • [{file_name}]({link})\n"
-                message += f"`Size  :` `{humanbytes(f_size)}`\n"
+                message += f"`Size   :` `{humanbytes(result[0])}`\n"
             result.append(files)
         if len(result) >= page_size:
             break
