f7add69c188c7f8ce3833b6c4dac01a58d8e9c02
arshsisodiya <arshsisodiya@gmail.com>
2021-07-10 08:12:02 +0530

    code cleanup in gdf chck module

--- userbot/modules/gdrive.py
+++ userbot/modules/gdrive.py
@@ -934,7 +934,7 @@ async def google_drive_managers(gdrive):
             )
             if mimeType != "application/vnd.google-apps.folder":
                 msg += f"`Size  :` `{humanbytes(f_size)}`\n"
-                msg += f"`Link  :` ({downloadURL})\n\n"
+                msg += f"`Link  :` {downloadURL}\n\n"
             else:
                 msg += f"`URL   :` [Open]({webViewLink})\n\n"
             if description:
