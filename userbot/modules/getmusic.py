ba577e8e5de9ec51b68e46d6d039f677db20650a
arshsisodiya <arshsisodiya@gmail.com>
2021-07-13 18:29:49 +0530

    fixup

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -100,7 +100,7 @@ async def _(event):
         await event.edit("`Enter a valid link to download from`")
     elif "playlist" in d_link:
         await event.edit("`Playlist Downloading...`")
-        await conv.send_message("`You are downloading A playlist the bot will only send first song to the current chat, you can find remaining songs in (@MusicsHunterBot) chat`")
+        await client.send_message("`You are downloading A playlist the bot will only send first song to the current chat, you can find remaining songs in (@MusicsHunterBot) chat`")
     else:
         await event.edit("`Downloading...`")
     chat = "@MusicsHunterBot"
