f890503899080014040d1058990fb33078e39e8a
arshsisodiya <arshsisodiya@gmail.com>
2021-07-13 15:40:45 +0530

    this might fix the downloading playlist error

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -108,12 +108,13 @@ async def _(event):
                 msg = await conv.send_message(d_link)
                 details = await conv.get_response()
                 song = await conv.get_response()
+                song2 = await conv.get_response()
                 """- don't spam notif -"""
                 await bot.send_read_acknowledge(conv.chat_id)
             except YouBlockedUserError:
-                await conv.send_message("/start")
+                await event.edit("`Unblock `@MusicsHunterBot` and retry`")
                 return
-            await bot.send_file(event.chat_id, song, caption=details.text)
+            await bot.send_file(event.chat_id, song, song2, caption=details.text)
             await event.client.delete_messages(
                 conv.chat_id, [msg_start.id, response.id, msg.id, details.id, song.id]
             )
