565c9fd5f44e4d98d0bc9ada250aba443e539e9c
arshsisodiya <arshsisodiya@gmail.com>
2021-07-15 16:12:58 +0530

    added seprate module for auddbot

--- userbot/modules/sazam.py
+++ userbot/modules/sazam.py
@@ -13,65 +13,7 @@ from userbot.events import register
 from userbot.utils import media_type
 from userbot.utils.logger import logging
 
-@register(outgoing=True, pattern=r"^\.szm(?: |$)(.*)")
-async def shazamcmd(event):
-    "To reverse search song."
-    reply = await event.get_reply_message()
-    mediatype = media_type(reply)
-    if not reply or not mediatype or mediatype not in ["Voice", "Audio"]:
-        return await event.edit("__Reply to Voice clip or Audio clip to reverse search that song.__")
-    catevent = await event.edit("__Downloading the audio clip...__")
-    try:
-        for attr in getattr(reply.document, "attributes", []):
-            if isinstance(attr, types.DocumentAttributeFilename):
-                name = attr.file_name
-        dl = io.FileIO(name, "a")
-        await event.client.fast_download_file(
-            location=reply.document,
-            out=dl,
-        )
-        dl.close()
-        mp3_fileto_recognize = open(name, "rb").read()
-        shazam = Shazam(mp3_fileto_recognize)
-        recognize_generator = shazam.recognizeSong()
-        track = next(recognize_generator)[1]["track"]
-    except Exception as e:
-        return await event.edit("**Error while reverse searching song:**\n__{str(e)}__"
-        )
-    image = track["images"]["background"]
-    song = track["share"]["subject"]
-    await event.client.send_file(
-        event.chat_id, image, caption=f"**Song:** `{song}`", reply_to=reply
-    )
-    await event.delete()
 
-@register(outgoing=True, pattern=r"^\.shazam(?: |$)(.*)")
-async def _(event):
-    "To reverse search music by bot."
-    if not event.reply_to_msg_id:
-        return await event.edit("```Reply to an audio message.```")
-    reply_message = await event.get_reply_message()
-    chat = "@auddbot"
-    await event.edit("```Identifying the song```")
-    async with event.client.conversation(chat) as conv:
-        try:
-            await conv.send_message("/start")
-            await conv.get_response()
-            await conv.send_message(reply_message)
-            check = await conv.get_response()
-            if not check.text.startswith("Audio received"):
-                return await event.edit(
-                    "An error while identifying the song. Try to use a 5-10s long audio message."
-                )
-            await event.edit("Wait just a sec...")
-            result = await conv.get_response()
-            await event.client.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await event.edit("```Please unblock (@auddbot) and try again```")
-            return
-    namem = f"**Song Name : **`{result.text.splitlines()[0]}`\
-        \n\n**Details : **__{result.text.splitlines()[2]}__"
-    await event.edit(namem)
 
 @register(outgoing=True, pattern=r"^\.sid(?: |$)(.*)")
 async def _(event):
