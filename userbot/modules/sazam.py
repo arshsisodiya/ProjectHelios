84358981fb56cd5cd85cb1e834bd9eaf8e4ae366
arshsisodiya <arshsisodiya@gmail.com>
2021-07-15 15:30:39 +0530

    testing integration of SongIDbot for Reverse searching

--- userbot/modules/sazam.py
+++ userbot/modules/sazam.py
@@ -91,12 +91,8 @@ async def _(event):
                     "An error while identifying the song. Try to use a 5-10s long audio message."
                 )
             await event.edit("Wait just a sec...")
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=461083923)
-            )
-            response = await response
-            msg = response.message.message
-            await event.edit(msg)
+            result = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             await event.edit("```Please unblock (@SongIDbot) and try again```")
             return
@@ -104,6 +100,39 @@ async def _(event):
         \n\n**Details : **__{result.text.splitlines()[2]}__"
     await event.edit(namem)
 
+
+@register(outgoing=True, pattern=r"^\.sid2(?: |$)(.*)")
+async def _(event):
+    "To recognize a song."
+    if not event.reply_to_msg_id:
+        return await event.edit("Reply to any user's media message.")
+    reply_message = await event.get_reply_message()
+    if not reply_message.media:
+        return await event.edit(event, "reply to media file")
+    chat = "@SongIDbot"
+    if reply_message.sender.bot:
+        return await event.edit(event, "Reply to actual users message.")
+    await event.edit("recognizeing this media")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=461083923)
+            )
+            await event.client.forward_messages(chat, reply_message)
+            response = await response
+        except YouBlockedUserError:
+            await event.edit("unblock @SongIDbot and try again")
+            return
+        if response.text.startswith("Perfect Match."):
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=461083923)
+            )
+            response = await response
+            msg = response.message.message
+            await event.edit(msg)
+        else:
+            await event.edit("sorry, I couldnt find it")
+        await event.client.send_read_acknowledge(conv.chat_id)
 CMD_HELP.update(
     {
         "shazam": ">`.szm <reply to voice/audio>" "\nUsage: Reverse search audio file using shazam api"}
