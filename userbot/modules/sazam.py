1a7d75d5039eaaf2bcba592df542f657ffcc8ada
arshsisodiya <arshsisodiya@gmail.com>
2021-07-15 15:53:31 +0530

    fixing issues
    testing integration of SongIDbot for Reverse searching

--- userbot/modules/sazam.py
+++ userbot/modules/sazam.py
@@ -92,11 +92,12 @@ async def _(event):
                     "An error while identifying the song. Try to use a 5-10s long audio message."
                 )
             await event.edit("Wait just a sec...")
-            result = await conv.get_response()
+            await conv.get_response()
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             await event.edit("```Please unblock (@SongIDbot) and try again```")
             return
+        await conv.get_response()
 
 @register(outgoing=True, pattern=r"^\.sid2(?: |$)(.*)")
 async def _(event):
@@ -110,26 +111,24 @@ async def _(event):
     if reply_message.sender.bot:
         return await event.edit(event, "Reply to actual users message.")
     await event.edit("recognizeing this media")
-    async with event.client.conversation(chat) as conv:
+    async with bot.conversation(chat) as conv:
         try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=461083923)
-            )
-            await event.client.forward_messages(chat, reply_message)
-            response = await response
+            msg_start = await conv.send_message("/start")
+            response = await conv.get_response()
+            msg = await conv.send_message(d_link)
+            details = await conv.get_response()
+            song = await conv.get_response()
+            """- don't spam notif -"""
+            await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await event.edit("unblock @SongIDbot and try again")
+            await event.edit("`Unblock `@SongIDbot` and retry`")
             return
-        if response.text.startswith("Perfect Match."):
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=461083923)
-            )
-            response = await response
-            msg = response.message.message
-            await event.edit(msg)
-        else:
-            await event.edit("sorry, I couldnt find it")
-        await event.client.send_read_acknowledge(conv.chat_id)
+        await bot.send_file(event.chat_id, song, caption=details.text)
+        await event.client.delete_messages(
+            conv.chat_id, [msg_start.id, response.id, msg.id, details.id, song.id]
+        )
+        await event.delete()
+
 CMD_HELP.update(
     {
         "shazam": ">`.szm <reply to voice/audio>" "\nUsage: Reverse search audio file using shazam api"}
