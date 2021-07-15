920449537047ce8429dfa646e6a672277ef93553
arshsisodiya <arshsisodiya@gmail.com>
2021-07-15 18:29:14 +0530

    refacor

--- userbot/modules/alldownloader.py
+++ userbot/modules/alldownloader.py
@@ -18,27 +18,24 @@ async def _(event):
         if reply_message.sender.bot:
             return await event.edit(event, "Reply to actual users message.")
         await event.edit("downloading this media")
-        async with event.client.conversation(chat) as conv:
+        async with bot.conversation(chat) as conv:
             try:
-                response = conv.wait_event(
-                    events.NewMessage(incoming=True,)
-                )
-                await event.client.send_message(chat, reply_message)
-                response = await response
+                msg_start = await conv.send_message("/start")
+                response = await conv.get_response()
+                msg = await conv.send_message(d_link)
+                details = await conv.get_response()
+                song = await conv.get_response()
+                """"- don't spam notif -"""
+                await bot.send_read_acknowledge(conv.chat_id)
             except YouBlockedUserError:
                 await event.edit("unblock @allsaverbot and try again")
                 return
-            if response.text.startswith("Follow to @allsavernews channel, in order to keep up to date with all the bot news."):
-                response = conv.wait_event(
-                    events.NewMessage(incoming=True,)
-                )
-                response = await response
-                msg = response.message
-                event.client.download_media(chat, msg)
-            else:
-                await event.edit("sorry, I couldnt find it")
-            await event.client.send_read_acknowledge(conv.chat_id)
-
+            await bot.send_file(event.chat_id, song, caption=details.text)
+            await event.client.delete_messages(
+                conv.chat_id, [msg_start.id, response.id, msg.id, details.id, song.id]
+            )
+            await event.delete()
+    
 
 
 CMD_HELP.update(
