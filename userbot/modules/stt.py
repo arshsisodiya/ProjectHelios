540228d0b42bc9eb54905c85e060b9bf48a01801
arshsisodiya <arshsisodiya@gmail.com>
2021-07-16 02:15:00 +0530

    added module for speech to text

--- userbot/modules/stt.py
+++ userbot/modules/stt.py
@@ -1,3 +1,4 @@
+# Copyright (C) 2021 arshsisodiya
 import asyncio
 from asyncio.exceptions import TimeoutError
 from telethon import events
@@ -22,14 +23,15 @@ async def _(event):
             msg_start = await conv.send_message("/start")
             await event.client.forward_messages(chat, reply_message)
             response = await conv.get_response()
+            result = await conv.get_response()
             """- don't spam notif -"""
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             await event.edit("`Unblock `@voicybot` and retry`")
             return
-        await event.client.send_message(event.chat_id, response)
+        await event.client.send_message(event.chat_id, result)
         await event.client.delete_messages(
-            conv.chat_id, [msg_start.id, response.id, msg.id, ]
+            conv.chat_id, [msg_start.id, response.id, result.id]
         )
         await event.delete()
 
