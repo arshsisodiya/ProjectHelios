c7bab14e47a9e6046ac8acbd2af1bac06b6a808e
arshsisodiya <arshsisodiya@gmail.com>
2021-07-17 00:12:02 +0530

    support for text to pdf

--- userbot/modules/pdfutils.py
+++ userbot/modules/pdfutils.py
@@ -106,3 +106,42 @@ async def _(event):
     except TimeoutError:
         return await event.edit(
             "`Error: `@pdfbot` is not responding try again after some time")
+
+    @register(outgoing=True, pattern=r"^\.pdf2text(?: |$)(.*)")
+    async def _(event):
+        if event.fwd_from:
+            return
+        msg_link = await event.get_reply_message()
+        d_link = event.pattern_match.group(1)
+
+        if msg_link:
+            d_link = msg_link.text
+            await event.edit("`Converting into pdf`")
+
+        else:
+            await event.edit("`Converting into pdf`")
+        chat = "@pdfbot"
+        try:
+            async with bot.conversation(chat) as conv:
+                try:
+                    msg_start = await conv.send_message("/start")
+                    response = await conv.get_response()
+                    text = await conv.send_message("/text")
+                    response2 = await conv.get_response()
+                    msg = await conv.send_message(d_link)
+                    font = await conv.send_message("Roboto")
+                    cnfrm = await conv.get_response()
+                    pdf = await conv.get_response()
+                    """- don't spam notif -"""
+                    await bot.send_read_acknowledge(conv.chat_id)
+                except YouBlockedUserError:
+                    await event.edit("`Unblock `@pdfbot` and retry`")
+                    return
+                await bot.send_file(event.chat_id, pdf)
+                await event.client.delete_messages(
+                    conv.chat_id, [msg_start.id, response.id, msg.id,text.id, response.id, response2.id, cnfrm.id, pdf.id]
+                )
+                await event.delete()
+        except TimeoutError:
+            return await event.edit(
+                "`Error: ")
