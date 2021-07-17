1b091b0544480958e5f1ebfef96f4fbcd6397e7d
arshsisodiya <arshsisodiya@gmail.com>
2021-07-17 13:43:45 +0530

    fixing major errors

--- userbot/modules/pdfutils.py
+++ userbot/modules/pdfutils.py
@@ -44,36 +44,36 @@ async def _(event):
         return await event.edit(
             "`Error: `@pdfbot` is not responding try again after some time")
 
-@register(outgoing=True, pattern=r"^\.pdf (?:(photo2)|(.*) - (.*))")
+@register(outgoing=True, pattern=r"^\.pdfphoto(?: |$)(.*)")
 async def _(event):
-    "."
-    if event.pattern_match.group(1) == "photo2":
-     if not event.reply_to_msg_id:
-        return await event.edit("Reply to any user's media message.")
-    reply_message = await event.get_reply_message()
-    if not reply_message.media:
-        return await event.edit(event, "reply to media file")
-    chat = "@pdfbot"
-    await event.edit("converting your photos into pdf please send another photo via command `.pdf photo2` or send final photo using command `.pdf photof`")
+    if event.fwd_from:
+        return
+    msg_link = await event.get_reply_message()
+    d_link = event.pattern_match.group(1)
 
+    if msg_link:
+        d_link = msg_link.media
+        await event.edit("`sending...`")
+    chat = "@pdfbot"
+    await event.edit("`sending.........`")
     try:
         async with bot.conversation(chat) as conv:
             try:
-                photo = await event.client.send_message(chat, reply_message)
+                msg = await conv.send_message(d_link)
+                await sleep(2)
+                torrent = await conv.get_response()
                 """- don't spam notif -"""
                 await bot.send_read_acknowledge(conv.chat_id)
             except YouBlockedUserError:
                 await event.edit("`Unblock `@pdfbot` and retry`")
                 return
-            #await event.client.send_message(event.chat_id, response)
-            await sleep(30)
+            await event.client.send_message(event.chat_id, torrent,)
             await event.client.delete_messages(
-                conv.chat_id, [photo.id]
+                conv.chat_id, [msg.id, torrent.id]
             )
             await event.delete()
     except TimeoutError:
-        return await event.edit(
-            "`Error: `@pdfbot` is not responding try again after some time")
+        return await event.edit("`Error: `@pdfbot` is not responding please try again later")
 
 @register(outgoing=True, pattern=r"^\.pdf (?:(photof)|(.*) - (.*))")
 async def _(event):
@@ -90,7 +90,7 @@ async def _(event):
         async with bot.conversation(chat) as conv:
             try:
                 photo = await event.client.send_message(chat, reply_message)
-                donecmd = await conv.send_message("/Done")
+                donecmd = await conv.send_message("To PDF")
                 response = await conv.get_response()
                 fresponse = await conv.get_response()
                 """- don't spam notif -"""
@@ -107,41 +107,3 @@ async def _(event):
         return await event.edit(
             "`Error: `@pdfbot` is not responding try again after some time")
 
-    @register(outgoing=True, pattern=r"^\.pdf2text(?: |$)(.*)")
-    async def _(event):
-        if event.fwd_from:
-            return
-        msg_link = await event.get_reply_message()
-        d_link = event.pattern_match.group(1)
-
-        if msg_link:
-            d_link = msg_link.text
-            await event.edit("`Converting into pdf`")
-
-        else:
-            await event.edit("`Converting into pdf`")
-        chat = "@pdfbot"
-        try:
-            async with bot.conversation(chat) as conv:
-                try:
-                    msg_start = await conv.send_message("/start")
-                    response = await conv.get_response()
-                    text = await conv.send_message("/text")
-                    response2 = await conv.get_response()
-                    msg = await conv.send_message(d_link)
-                    font = await conv.send_message("Roboto")
-                    cnfrm = await conv.get_response()
-                    pdf = await conv.get_response()
-                    """- don't spam notif -"""
-                    await bot.send_read_acknowledge(conv.chat_id)
-                except YouBlockedUserError:
-                    await event.edit("`Unblock `@pdfbot` and retry`")
-                    return
-                await bot.send_file(event.chat_id, pdf)
-                await event.client.delete_messages(
-                    conv.chat_id, [msg_start.id, response.id, msg.id,text.id, response.id, response2.id, cnfrm.id, pdf.id]
-                )
-                await event.delete()
-        except TimeoutError:
-            return await event.edit(
-                "`Error: ")
