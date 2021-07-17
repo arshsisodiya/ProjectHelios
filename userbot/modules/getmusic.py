65c859558d4131fa19d1b8eb160af84dddd8b3cd
arshsisodiya <arshsisodiya@gmail.com>
2021-07-17 16:36:30 +0530

    testing forwarding multiple msgs

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -115,15 +115,13 @@ async def _(event):
                 response = await conv.get_response()
                 msg = await conv.send_message(d_link)
                 details = await conv.get_response()
-                song = await conv.get_response()
-                song2 = await conv.get_response()
-                song3 = await conv.get_response()
+                messages= [await conv.get_response(),await conv.get_response(), await conv.get_response()]
                 """- don't spam notif -"""
                 await bot.send_read_acknowledge(conv.chat_id)
             except YouBlockedUserError:
                 await event.edit("`Unblock `@MusicsHunterBot` and retry`")
                 return
-            await bot.forward_messages(event.chat_id, song, song2, song3)
+            await bot.forward_messages(event.chat_id, messages)
             await event.client.delete_messages(
                 conv.chat_id, [msg_start.id, response.id, msg.id, details.id, song.id]
             )
