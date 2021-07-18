6f46ea3cc76e2b438dddb2fd3b07e7c645b38ee6
arshsisodiya <arshsisodiya@gmail.com>
2021-07-18 15:11:49 +0530

    added link shortner module
    now user can get short url for any long link using shorturlbot

---
+++ userbot/modules/link_shortner.py
@@ -0,0 +1,56 @@
+#Copyright @arshsisodiya
+#https://github.com/arshsisodiya
+#https://twitter.com/arshsisodiya
+
+#Created by arshsisodiya for ProjectHelios
+
+import asyncio
+from asyncio.exceptions import TimeoutError
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from userbot import CMD_HELP, bot
+from userbot.events import register
+
+@register(outgoing=True, pattern=r"^\.short(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    msg_link = await event.get_reply_message()
+    query = event.pattern_match.group(1)
+
+    if msg_link:
+        query = msg_link.text
+        await event.edit("`Shortning your link.....`")
+    else:
+        await event.edit("`Shortning your link......`")
+    chat = "@ShortUrlBot"
+    try:
+        async with bot.conversation(chat) as conv:
+            try:
+                msg_start = await conv.send_message("/start")
+                response = await conv.get_response()
+                send_query = await conv.send_message(query)
+                garbagetext = await conv.get_response()
+                shorturl = await conv.get_response()
+                garbagetext1 = await conv.get_response()
+                garbagetext2 = await conv.get_response()
+                """- don't spam notif -"""
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await event.edit("`Unblock `@ShortUrlBot` and retry`")
+                return
+            await event.client.send_message(event.chat_id, shorturl)
+            await event.client.delete_messages(
+                conv.chat_id, [msg_start.id, response.id, send_query.id, video.id, garbagetext.id, garbagetext1.id, garbagetext2.id]
+            )
+            await event.delete()
+    except TimeoutError:
+        return await event.edit("`Error: `@ShortUrlBot` is not responding")
+CMD_HELP.update(
+    {
+        "`link shortner`": ".short <url>"
+                "\nUsage: Reply or paste a link to get "
+                "short url using `@ShortUrlBot`"
+    }
+)
+
