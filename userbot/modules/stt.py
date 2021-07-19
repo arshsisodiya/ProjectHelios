f5e712187d7eefc1bdf55d0342d2d4ccdecd6a89
arshsisodiya <arshsisodiya@gmail.com>
2021-07-20 00:54:12 +0530

    stt: removed voicybot sponser text

--- userbot/modules/stt.py
+++ userbot/modules/stt.py
@@ -31,7 +31,9 @@ async def _(event):
         except YouBlockedUserError:
             await event.edit("`Unblock `@voicybot` and retry`")
             return
-        await event.client.send_message(event.chat_id, result)
+        text = f"`{result.text.splitlines()[0]}`"
+        await event.edit(text)
+        #await event.client.send_message(event.chat_id, result)
         await event.client.delete_messages(
             conv.chat_id, [msg_start.id, response.id, result.id, speech.id]
         )
