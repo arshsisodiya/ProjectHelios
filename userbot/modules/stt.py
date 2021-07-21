5cc2d3ec693c49d1a8a5a0eb5445a53f93f22419
arshsisodiya <arshsisodiya@gmail.com>
2021-07-21 22:37:25 +0530

    stt: added support for hindi language

--- userbot/modules/stt.py
+++ userbot/modules/stt.py
@@ -9,7 +9,6 @@ from userbot.events import register
 
 @register(outgoing=True, pattern=r"^\.stt(?: |$)(.*)")
 async def _(event):
-    "To recognize a image."
     if not event.reply_to_msg_id:
         return await event.edit("Reply to any user's media message.")
     reply_message = await event.get_reply_message()
@@ -22,7 +21,7 @@ async def _(event):
     async with event.client.conversation(chat) as conv:
         try:
             msg_start = await conv.send_message("/start")
-            await event.edit("voice recognition is initiated.....")
+            await event.edit("english voice recognition is initiated.....")
             speech = await event.client.send_message(chat, reply_message)
             response = await conv.get_response()
             await sleep(4)
@@ -38,12 +37,46 @@ async def _(event):
             conv.chat_id, [msg_start.id, response.id, result.id, speech.id]
         )
 
+@register(outgoing=True, pattern=r"^\.stthindi(?: |$)(.*)")
+async def _(event):
+    if not event.reply_to_msg_id:
+        return await event.edit("Reply to any user's media message.")
+    reply_message = await event.get_reply_message()
+    if not reply_message.media:
+        return await event.edit(event, "reply to media file")
+    chat = "@voicybot"
+    if reply_message.sender.bot:
+        return await event.edit(event, "Reply to actual users message.")
+    await event.edit("identifying the media")
+    async with event.client.conversation(chat) as conv:
+        try:
+            msg_start = await conv.send_message("/start")
+            await event.edit("hindi voice recognition is initiated.....")
+            langsettohindi = await event.client.send_message("/l hindi")
+            thumpsup = await conv.get_response()
+            speech = await event.client.send_message(chat, reply_message)
+            response = await conv.get_response()
+            await sleep(4)
+            result = await conv.get_response()
+            """- don't spam notif -"""
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.edit("`Unblock `@voicybot` and retry`")
+            return
+        text = f"`{result.text.splitlines()[0]}`"
+        await event.edit(text)
+        await event.client.delete_messages(
+            conv.chat_id, [msg_start.id, response.id, result.id, speech.id, langsettohindi.id, thumpsup.id]
+        )
+
 
 CMD_HELP.update(
     {
-        "stt": ".speech to text "
+        "stt": ".stt"
                  "\nUsage: Reply to a audio file to convert it into text "
                  "using `@voicybot`"
+                 "\n\n stthindi"
+                 "\nusage: Convert a hindi voice speech hindi text"
     }
 )
 
