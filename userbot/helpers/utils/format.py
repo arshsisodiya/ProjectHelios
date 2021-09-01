fce571e58222f61923efde251cda2f749a6af2e8
arshsisodiya <arshsisodiya@gmail.com>
2021-09-01 21:20:18 +0530

    fixup imports in format.py

--- userbot/helpers/utils/format.py
+++ userbot/helpers/utils/format.py
@@ -6,7 +6,7 @@ from telethon.tl.tlobject import TLObject
 from telethon.tl.types import MessageEntityPre
 from telethon.utils import add_surrogate
 
-from ..functions.utils import utc_to_local
+from .helpers.utils import utc_to_local
 from .paste import pastetext
 
 
