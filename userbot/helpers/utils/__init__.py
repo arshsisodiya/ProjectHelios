e6d0ca150b305136894e6b5b2c32e44c465e183d
arshsisodiya <arshsisodiya@gmail.com>
2021-09-01 20:19:56 +0530

    added new module for chat summery

---
+++ userbot/helpers/utils/__init__.py
@@ -0,0 +1,16 @@
+from .extdl import *
+from .paste import *
+
+flag = True
+check = 0
+while flag:
+    try:
+        from . import format as _format
+        from .format import *
+
+        break
+    except ModuleNotFoundError as e:
+        install_pip(e.name)
+        check += 1
+        if check > 5:
+            break
